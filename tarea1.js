6f5ac7f5c35ee2ad7bc32e0822a8024bac3963b0
JeampauÃl Salazar Castillo <jpaulsalazar28@gmail.com>
2021-06-22 17:33:18 -0600

    se corrigio error de variable

--- tarea1.js
+++ tarea1.js
@@ -1,6 +1,5 @@
 exports.fruits = function (fruit){
     let fruitsList = ["Manzana", "Pera", "Uva", "Sandía", "Banano", "Melocotón", "Piña", "Maracuyá", "Mandarina", "Mango"]
-    let msj = ""
     let exist = false
     for (let i = 0; i < fruitsList.length; i++) {
         if (fruit == fruitsList[i]) {
@@ -11,6 +10,7 @@ exports.fruits = function (fruit){
 }
 
 function listResult(exist){
+    let msj = ""
     if (exist) {
         msj = "la fruta se encuentra en la lista"
     }else{
